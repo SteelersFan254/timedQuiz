2eebae01b82bf3def07f60f379dc7dad4180caf1
Ryan Borja <rcborja.school@gmail.com>
2021-03-02 17:07:13 -0600

    made the questions

---
+++ script.js
@@ -0,0 +1,32 @@
+var questions = {
+    "question0" : {
+        "questionNum": 1,
+        "question" : "How many Super Bowl appearances do the Steelers have?",
+        "answers" :  [6,7,8,9],
+        "correct" : 8
+    },
+    "question1" : {
+        "questionNum": 2,
+        "question" : "How many Super Bowl wins do the Steelers have?",
+        "answers" :  [5,6,7,8],
+        "correct" : 6
+    },
+    "question2" : {
+        "questionNum": 3,
+        "question" : "Which running back has the most rushing yards in franchise history?",
+        "answers" :  ["Franco Harris", "Jerome Bettis", "Willie Parker", "Le'Veon Bell"],
+        "correct" : "Franco Harris"
+    },
+    "question3" : {
+        "questionNum": 4,
+        "question" : "Which player has the most recieving yards in franchise history?",
+        "answers" :  ["Lynn Swann", "John Stallwordth", "Hines Ward", "Antonio Brown"],
+        "correct" : "Hines Ward"
+    },
+    "question4" : {
+        "questionNum": 5,
+        "question" : "Who owns the Pittsburgh Steelers franchise?",
+        "answers" :  ["Mike Tomlin", "Dan Rooney", "Kevin Colbert", "Bill Cowher"],
+        "correct" : "Dan Rooney"
+    },
+}
